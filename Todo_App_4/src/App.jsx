c84fc2f113dc7437c5be931914d89463ea8accb0
Haresh9861 <hareshnayak9861@gmail.com>
2024-06-26 10:24:35 +0000

    Adding

--- Todo_App_4/src/App.jsx
+++ Todo_App_4/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import { RecoilRoot, useRecoilState } from 'recoil';
+import { todosAtomFamily } from './atoms';
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <RecoilRoot>
+    <Todo id={1}/>
+    <Todo id={2} />
+  </RecoilRoot>
+}
+
+function Todo({id}) {
+   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
 
   return (
-   <div>
-    
-   </div>
+    <>
+      {todo.title}
+      {todo.description}
+      <br />
+    </>
   )
 }
 
